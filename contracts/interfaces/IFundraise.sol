7e51f2c6d5f5e63fc18c86cfb0dece084b24ff69
anonymous <>
2026-01-19 13:39:02 +0300

    interface for fundraise

---
+++ contracts/interfaces/IFundraise.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IFundraise {
+    enum Stage {
+        ComingSoon,
+        Open,
+        Canceled,
+        PreFunded,
+        Funded,
+        Repaid
+    }
+
+    struct Project {
+        uint256 hardCap;
+        uint256 softCap;
+        uint256 totalInvested;
+        uint256 startAt;
+        uint256 preFundDuration;
+        uint256 investorInterestRate;
+        uint256 openStageEndAt;
+        InnerProjectStruct innerStruct;
+    }
+
+    struct InnerProjectStruct {
+        uint256 platformInterestRate;
+        uint256 totalRepaid;
+        address borrower;
+        uint256 fundedTime;
+        IERC20 loanToken;
+        Stage stage;
+    }
+
+    struct InvestorInfo {
+        uint256 investedAmount;
+        uint256 totalClaimed;
+    }
+
+    function projects(uint256 _projectId) external view returns (Project memory);
+
+    function investorInfo(address _investor, uint256 _projectId) external view returns (InvestorInfo memory);
+
+    function transferInvestment(uint256 _projectId, address _from, address _to, uint256 _amount) external;
+
+    function BASIS_POINTS() external view returns (uint256);
+}
